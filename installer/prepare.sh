938c8ebec1d9b3b2adce7de6db5759bdbdcd84d7
Nathan Bourgeois <iridescentrosesfall@gmail.com>
2020-06-14 19:19:45 -0400

    Forgot to uncomment stuff

--- installer/prepare.sh
+++ installer/prepare.sh
@@ -1,15 +1,15 @@
 # PSP-LLVM Installation on APT based linux.
 
 #Install Rust
-# curl https://sh.rustup.rs -sSf | sh
+curl https://sh.rustup.rs -sSf | sh
 
 source $HOME/.cargo/env
 
-# rustup set profile complete
-# rustup toolchain install nightly
-# rustup default nightly && rustup component add rust-src
-# rustup update
-# cargo install cargo-psp
+rustup set profile complete
+rustup toolchain install nightly
+rustup default nightly && rustup component add rust-src
+rustup update
+cargo install cargo-psp
 
 
 sudo ./installer/prepare2.sh
