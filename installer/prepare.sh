858146a9db65ca1a7f6f652396bf922f11670901
= <catapultit@me.com>
2020-06-15 10:33:31 +1000

    Update Readme.md and tidy up scripts for install

--- installer/prepare.sh
+++ installer/prepare.sh
@@ -1,6 +1,6 @@
 # PSP-LLVM Installation on APT based linux.
 
-#Install Rust
+#Install Rust - No sudo required
 curl https://sh.rustup.rs -sSf | sh
 
 source $HOME/.cargo/env
@@ -11,5 +11,6 @@ rustup default nightly && rustup component add rust-src
 rustup update
 cargo install cargo-psp
 
-
+echo "Fetching Clang / LLVM from official repo -  https://apt.llvm.org/llvm.sh"
+echo "Sudo access is required to continue"
 sudo ./installer/prepare2.sh
--- installer/prepare2.sh
+++ installer/prepare2.sh
@@ -2,19 +2,14 @@
 bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)"
 
 # LLVM
-apt-get install libllvm-10-ocaml-dev libllvm10 llvm-10 llvm-10-dev llvm-10-doc llvm-10-examples llvm-10-runtime
+apt-get -y install libllvm-10-ocaml-dev libllvm10 llvm-10 llvm-10-dev llvm-10-doc llvm-10-examples llvm-10-runtime
 # Clang and co
-apt-get install clang-10 clang-tools-10 clang-10-doc libclang-common-10-dev libclang-10-dev libclang1-10 clang-format-10 python-clang-10 clangd-10
-# libfuzzer
-apt-get install libfuzzer-10-dev
-# lldb
-apt-get install lldb-10
-# lld (linker)
-apt-get install lld-10
-# libc++
-apt-get install libc++-10-dev libc++abi-10-dev
-# OpenMP
-apt-get install libomp-10-dev
+apt-get -y install clang-10 clang-tools-10 clang-10-doc libclang-common-10-dev libclang-10-dev libclang1-10 clang-format-10 python-clang-10 clangd-10
+# libfuzzer, lldb, lld (linker), libc++, OpenMP
+apt-get -y install libfuzzer-10-dev lldb-10 lld-10 libc++-10-dev libc++abi-10-dev libomp-10-dev
 
-#Cool, dependencies should have been installed.
 echo "Dependencies have been installed!"
+
+echo "Setting up PSPDev Environment"
+mkdir "$HOME/.pspdev"
+cp -r "$PWD/pspdev/" "$HOME/.pspdev/"
